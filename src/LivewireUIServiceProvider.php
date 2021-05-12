da1ff184a49e773edb2dc0eefc45df677d9bc5b3
Pascal Schwientek <pascalschwientek@users.noreply.github.com>
2021-05-12 09:43:38 +0200

    Update LivewireUIServiceProvider.php

--- src/LivewireUIServiceProvider.php
+++ src/LivewireUIServiceProvider.php
@@ -23,7 +23,7 @@ class LivewireUIServiceProvider extends ServiceProvider
     {
         return collect($this->app->getProviders('LivewireUI\Modal\LivewireModalServiceProvider'))
             ->map(function ($provider) {
-                return collect($provider::$scripts)->map(fn($file) => asset("/vendor/livewire-ui/{$file}"));
+                return collect($provider::$scripts)->map(fn($file) => global_asset("/vendor/livewire-ui/{$file}"));
             })->flatten()->map(fn($scriptUrl) => '<script src="' . $scriptUrl . '"></script>')->implode(PHP_EOL);
     }
 }
